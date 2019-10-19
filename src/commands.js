75a14ecb2fab58af404e112d039b25344684d5cc
siso9to <yonepin@gmail.com>
2019-10-20 00:27:52 +0900

    add slack command

---
+++ src/commands.js
@@ -0,0 +1,21 @@
+exports.handler = function (event, context, callback) {
+  const response = {
+    "text": "オススメの泣けるアニメ",
+    "attachements": [
+      {
+        "text": "CLANNAD AFTER STORY"
+      },
+      {
+        "text": "ヴァイオレット・エヴァーガーデン"
+      }
+    ]
+  }
+
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(response)
+  })
+}
