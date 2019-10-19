62408fd7c0d1d4b506144bce76a2d02bc51f3da2
siso9to <yonepin@gmail.com>
2019-10-20 00:47:30 +0900

    fix

--- src/event_handler.js
+++ src/event_handler.js
@@ -1,6 +1,6 @@
 exports.handler = function (event, context, callback) {
   const body = JSON.parse(event.body)
-  console.log(JSON.stringify(body, bull, 4))
+  console.log(JSON.stringify(body, null, 4))
 
   callback(null, {
     statusCode: 200,
