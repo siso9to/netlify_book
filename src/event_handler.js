cb0b0fe6a7aded8a1f8ffb1706b35dae057f2c5f
siso9to <yonepin@gmail.com>
2019-10-20 01:08:29 +0900

    add create channel event handler

--- src/event_handler.js
+++ src/event_handler.js
@@ -1,12 +1,20 @@
-exports.handler = function (event, context, callback) {
+import { WebClient } from '@slack/client'
+
+exports.handler = async function (event, context, callback) {
   const body = JSON.parse(event.body)
+  const slackEvent = body.event
   console.log(JSON.stringify(body, null, 4))
+
+  if (slackEvent && slackEvent.type === 'channel_created') {
+    const web = new WebClient(process.env.SLACK_TOKEN)
+    await web.chat.postMessage({
+      channel: 'slack_test',
+      text: `チャンネルが追加されました :<#${slackEvent.channel.id}>`
+    })
+  }
 
   callback(null, {
     statusCode: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
     body: body.challenge
   })
 }
